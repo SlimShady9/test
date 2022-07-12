7cde68681880a346a9d6a508bc04fc10c5db32aa
Juan David Quintero <slim.shady99q@gmail.com>
2022-07-12 13:41:31 -0500

    tarea de carta pendiente

--- resources/js/Layouts/Base.jsx
+++ resources/js/Layouts/Base.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
+import Card from "@/Components/Card";
 
 export default function Base({ children }) {
     return (
         <>
             <Header />
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">_
-            {children}
-            </div>
+            <Card>{children}</Card>
             <Footer />
         </>
     );
