40f921d8e7efa82918c40140ec33613d16add506
Juandaflorez11 <60432466+Juandaflorez11@users.noreply.github.com>
2023-01-16 02:46:17 -0500

    Posibles nuevos estados del servicio comentados (sin enumerar)

--- resources/js/Constants/EstadoServiciosEnum.js
+++ resources/js/Constants/EstadoServiciosEnum.js
@@ -1,8 +1,13 @@
 const EstadoServiciosEnum = Object.freeze({
     SERVICIOS_INCIADO: 1,
-    SERVICIO_DIRECCION_CONFIRMADA: 2,
+    SERVICIO_MENSAJERIA: 0, //Datos de Origen del envío
+    SERVICIO_DIRECCION_CONFIRMADA: 2, //Dirección de origen
+    SERVICIO_MENSAJERIA_ESPECIAL: 0, //Selecciona el/los motivos de condicones especiales como en el excel (frágil, confidencial, etc.)
+    SERVICIO_CONTENIDO_DECLARADO: 0, //Datos del contenido para cualquier mensajería
+    SERVICIO_LOGISTICA: 0, //Campos adicionales para solicitud directa
+    SERVICIO_GESTION_DOCUMENTAL: 0, //Campos adicionales para solicitud directa
     SERVICIO_USUARIOS_ASIGNADOS: 3,
-    SERVICIO_CON_DETALLE: 4,
+    SERVICIO_CON_DETALLE: 4, //Tareas (task) asignadas
     SERVICIO_PENDIENTE: 5,
     SERVICIO_APROBADO: 6,
     SERVICIO_RECHAZADO: 7,
