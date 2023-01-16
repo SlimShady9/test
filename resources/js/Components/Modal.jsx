11cbe19fbbc962e84d15b5a7d8793ac57759c85d
SlimShady9 <slim.shady99q@gmail.com>
2023-01-15 19:56:28 -0500

    gabo deja de trolear el modal

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -22,38 +22,41 @@ function Modal({ children, show, onHide, title }) {
 
     return (
         <>
-            <div
-                className={`absolute w-full h-full top-0 bottom-0 right-0 left-0 z-20
-                place-items-center ${showAnim.modal}`}
-            >
-                <Card className={"relative translate-y-1/3 tracking-widest m-auto"}>
-                    <h1 className="absolute top-0 left-0 my-4 mx-6 font-extrabold text-lg">
-                        {title}
-                    </h1>
-                    <button
-                        className="absolute top-0 right-0 m-4 z-10"
-                        onClick={onHide}
+            {show && (
+                <>
+                    <div
+                        className={`fixed w-screen h-screen top-0 bottom-0 right-0 left-0 z-20 overflow-scroll ${showAnim.modal}`}
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 hover:text-gray-dark text-gray-light transition-colors duration-75 "
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </button>
-                    <div className="mt-6">{children}</div>
-                </Card>
-            </div>
-            <div
-                className={`fixed h-full w-full inset-0 bg-gray-dark
+                        <Card className={"relative mx-auto my-0 sm:my-4"}>
+                            <h1 className="absolute top-0 left-0 my-4 mx-6 font-extrabold text-lg">
+                                {title}
+                            </h1>
+                            <button
+                                className="absolute top-0 right-0 m-4 z-10"
+                                onClick={onHide}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5 hover:text-gray-dark text-gray-light transition-colors duration-75 "
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </button>
+                            <div className="mt-6">{children}</div>
+                        </Card>
+                    </div>
+                    <div
+                        className={`fixed h-full w-full inset-0 bg-gray-dark
                 ${showAnim.backdrop}`}
-            />
+                    />
+                </>
+            )}
         </>
     );
 }
