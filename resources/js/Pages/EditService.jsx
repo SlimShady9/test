a47cb75a1259eabf85ac27a399cce8557ceed10b
Juan David <juandavidquinter@hotmail.com>
2023-03-03 23:58:03 -0500

    Ajuste

--- resources/js/Pages/EditService.jsx
+++ resources/js/Pages/EditService.jsx
@@ -36,7 +36,7 @@ function EditService(props) {
     );
 
     const [stateService, setStateService] = useState(
-        EstadoServiciosEnum.SERVICIO_DIRECCION_CONFIRMADA
+        EstadoServiciosEnum.SERVICIO_MENSAJERIA
     );
 
     const [dataLoaded, setDataLoaded] = useState(false);
