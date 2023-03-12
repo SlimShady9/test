0ca175944962c143155462613b54ea7cd080484a
Juandaflorez11 <60432466+Juandaflorez11@users.noreply.github.com>
2023-03-12 17:39:11 -0500

    Graph controller

---
+++ app/Http/Controllers/Graphs/GraphController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Service;
+use App\Models\Address;
+use App\Models\Parameter;
+use App\Models\TypeService;
+use App\Models\StateService;
+use Illuminate\Support\Facades\DB;
+
+use Barryvdh\Debugbar\Facade as Debugbar;
+
+class GraphController extends Controller {
+
+    public function index() {
+        
+    }
+
+    public function show($id) {
+       
+    }
+
+
+}
