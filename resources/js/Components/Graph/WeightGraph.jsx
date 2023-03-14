8ddbaa4b339342efce60f908207a84df94c7ce91
Juandaflorez11 <60432466+Juandaflorez11@users.noreply.github.com>
2023-03-13 21:15:32 -0500

    Weight graph

---
+++ resources/js/Components/Graph/WeightGraph.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import { costXWeight } from "@/Utils/FetchGraph";
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "top",
+        },
+        title: {
+            display: true,
+            text: "Chart.js Bar Chart",
+        },
+    },
+};
+
+const WeightGraph = () => {
+    const [loading, setLoading] = useState(false);
+
+    const [data, setData] = useState({
+        datasets: [
+            {
+                label: "Costo",
+                data: 0,
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+            {
+                label: "Precio",
+                data: 0,
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+            },
+        ],
+    });
+    useEffect(() => {
+        const fetchData = async () => {
+            const labelSet = [];
+            const dataSet1 = [];
+            const dataSet2 = [];
+            const dataSet3 = [];
+            const res = await costXWeight();
+            for (const val of res) {
+                if(val.weigh!=null && val.cost && val.price && val.profits){
+                dataSet1.push(val.cost);
+                dataSet2.push(val.price);
+                dataSet3.push(val.profits);
+                labelSet.push(val.weigh);
+                }
+                // labelSet.push(val.name)
+            }
+            setData({
+                labels: labelSet,
+                datasets: [
+                    {
+                        label: "Costo",
+                        data: dataSet1,
+                        backgroundColor: "rgba(255, 99, 132, 0.5)",
+                        borderColor: "rgba(255, 99, 132, 0.5)",
+                        type: "line",
+                    },
+                    {
+                        label: "Precio",
+                        data: dataSet2,
+                        backgroundColor: "rgba(53, 162, 235, 0.5)",
+                        borderColor: "rgba(53, 162, 235, 0.5)",
+                        type: "line",
+                    },
+                ],
+            });
+        };
+
+        fetchData();
+    }, []);
+    if (loading) return <p>Loading...</p>;
+    return (
+    <>
+    <div>
+    <h1 className="text-blue-primary text-3xl mb-1 font-bold  text-center ease-in duration-200">
+        Peso Total x Ventas
+    </h1>
+    <Bar 
+    className="bg-semiwhite rounded-xl shadow-xl"
+    data={data} 
+    options={options}/>
+    </div>
+    </>
+    )
+};
+export default WeightGraph;
--- resources/js/Pages/Graph.jsx
+++ resources/js/Pages/Graph.jsx
@@ -4,6 +4,7 @@ import Authenticated from "@/Layouts/Authenticated";
 import SalesGraph from "@/Components/Graph/SalesGraph";
 import ServiceGraph from "@/Components/Graph/ServiceGraph";
 import VolumeGraph from "@/Components/Graph/VolumeGraph";
+import WeightGraph from "@/Components/Graph/WeightGraph";
 import { getService } from "@/Utils/FetchService";
 
 export default function Graph(props) {
@@ -12,9 +13,10 @@ export default function Graph(props) {
         <>
             <Authenticated {...props}>
                     <div className="grid sm:grid-cols-2  mx-5 gap-5">
-                        <VolumeGraph />
-                        <SalesGraph />
-                        <ServiceGraph />
+                        <WeightGraph/>
+                        <VolumeGraph/>
+                        <SalesGraph/>
+                        <ServiceGraph/>
                     </div>
             </Authenticated>
         </>
