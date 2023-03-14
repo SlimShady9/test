fe30398182e079f7703aeab594f54ad5e055a908
Juandaflorez11 <60432466+Juandaflorez11@users.noreply.github.com>
2023-03-13 22:36:10 -0500

    Mostrar graphs trolls de gabo

--- resources/js/Pages/Graph.jsx
+++ resources/js/Pages/Graph.jsx
@@ -5,7 +5,14 @@ import SalesGraph from "@/Components/Graph/SalesGraph";
 import ServiceGraph from "@/Components/Graph/ServiceGraph";
 import VolumeGraph from "@/Components/Graph/VolumeGraph";
 import WeightGraph from "@/Components/Graph/WeightGraph";
-import PriceTServicePie from "@/Components/Graph/PriceTServicePie";
+import CostTContent from "@/Components/Graph/CostTContent";
+import CostTService from "@/Components/Graph/CostTService";
+import PriceTContent from "@/Components/Graph/PriceTContent";
+import PriceTService from "@/Components/Graph/PriceTService";
+import ProfitTContent from "@/Components/Graph/ProfitTContent";
+import ServiceStateService from "@/Components/Graph/ServiceStateService";
+import ServiceTService from "@/Components/Graph/ServiceTService";
+
 import { getService } from "@/Utils/FetchService";
 
 export default function Graph(props) {
@@ -15,10 +22,13 @@ export default function Graph(props) {
             <Authenticated {...props}>
                 <div className="">
                     <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3  mx-5 gap-5">
-                        <PriceTServicePie/>
-                        <PriceTServicePie/>
-                        <PriceTServicePie/>
-                        <PriceTServicePie/>
+                        <CostTContent/>
+                        <CostTService/>
+                        <PriceTContent/>
+                        <PriceTService/>
+                        <ProfitTContent/>
+                        <ServiceStateService/>
+                        <ServiceTService/>
                     </div>
             
                     <div className="grid sm:grid-cols-2  mx-5 gap-5">
