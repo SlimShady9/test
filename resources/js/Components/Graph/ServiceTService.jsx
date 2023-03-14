35c44ef54691217117116a84580fd96ca6d18c03
gabriel <gabriel.ortega@asopagos.com>
2023-03-13 22:27:37 -0500

    ready

---
+++ resources/js/Components/Graph/ServiceTService.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { ServiceXTypeServicePerc } from "@/Utils/FetchGraph";
+import { toStringTipoDeServiciosEnum } from "@/Constants/TipoDeServiciosEnum";
+
+const PriceTServicePie = () => {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "bottom",
+            },
+            title: {
+                display: true,
+                text: "Ingresos totales por tipo de servicio",
+            },
+        },
+    };
+    
+    const [loading, setLoading] = useState(false);
+
+    const [data, setData] = useState({
+        datasets: [
+            {
+                label: "Precio",
+                data: 0,
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+            },
+        ],
+    });
+    useEffect(() => {
+        const fetchData = async () => {
+            const labelSet = [];
+            const dataSet1 = [];
+            const res = await ServiceXTypeServicePerc();
+            for (const val of res) {
+                if(val.id!=null && val.id_type_service){
+                dataSet1.push(val.id);
+                labelSet.push(toStringTipoDeServiciosEnum(val.id_type_service));
+                }
+                // labelSet.push(val.name)
+            }
+            setData({
+                labels: labelSet,
+                datasets: [
+                    {
+                        label: "Precio",
+                        data: dataSet1,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.5)',
+                            'rgba(54, 162, 235, 0.5)',
+                            'rgba(255, 206, 86, 0.5)',
+                            'rgba(75, 192, 192, 0.5)',
+                            'rgba(153, 102, 255, 0.5)',
+                            'rgba(255, 159, 64, 0.5)',
+                          ],
+                        type: "pie",
+                    },
+                ],
+            });
+        };
+
+        fetchData();
+    }, []);
+    if (loading) return <p>Loading...</p>;
+    return (
+    <>
+    <div>
+    <h1 className="text-blue-primary text-3xl mb-1 font-bold  text-center ease-in duration-200">
+        Precio x Tipo de Servicio
+    </h1>
+    <Pie 
+    className="bg-semiwhite rounded-xl shadow-xl m-5"
+    data={data} 
+    options={options}
+    />
+    </div>
+    </>
+    )
+};
+export default PriceTServicePie;
