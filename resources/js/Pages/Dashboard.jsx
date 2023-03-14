d2ddb5709f043c09b49b70bb35c5a39640ea62fe
Juandaflorez11 <60432466+Juandaflorez11@users.noreply.github.com>
2023-03-13 23:06:00 -0500

    Nuevo Dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Authenticated from "@/Layouts/Authenticated";
 import { Head, Link } from "@inertiajs/inertia-react";
-
+import Welcome from "./Welcome";
+import Label from "@/Components/FormUtils/Label";
+import { TipoDeUsuariosEnum } from "@/Constants/TipoDeUsuariosEnum";
 export default function Dashboard(props) {
     return (
         <Authenticated
@@ -10,16 +12,20 @@ export default function Dashboard(props) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
+            <div className="py-5 px-5">
                 <div className="grid grid-cols-3 gap-4 max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    
-                        <Link
+                </div>
+                {(props.auth.user.id_t_user==TipoDeUsuariosEnum.ADMIN) && ( 
+                <div className="grid sm:grid-cols-3 text-center">
+                <Link
                             href={route("envios")}
                             className="p-3 bg-blue-400"
                         >
                             <div className="p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                     border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200">
-                            Ver envios
+                            <Label className={"text-2xl"}>
+                                Ventas
+                            </Label>
                             </div>
                         </Link>
                     
@@ -29,7 +35,9 @@ export default function Dashboard(props) {
                         >
                     <div className="p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                     border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200">
-                            Servicios
+                            <Label className={"text-2xl"}>
+                                Servicios
+                            </Label>
                             </div>
                         </Link>
                         <Link
@@ -38,11 +46,17 @@ export default function Dashboard(props) {
                         >
                     <div className="p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                     border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200">
-                            Usuarios
+                            <Label className={"text-2xl"}>
+                                Usuarios
+                            </Label>
                             </div>
                         </Link>
-                    
                 </div>
+                )}
+                <Welcome
+                    hasHeader={false}
+                    hasFooter={false}
+                />
             </div>
         </Authenticated>
     );
