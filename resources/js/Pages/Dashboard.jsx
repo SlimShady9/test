d3b46ffa9012225098016f104dc388e6f7817144
Juandaflorez11 <60432466+Juandaflorez11@users.noreply.github.com>
2023-04-09 12:46:56 -0500

    Arreglos dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -4,6 +4,15 @@ import { Head, Link } from "@inertiajs/inertia-react";
 import Welcome from "./Welcome";
 import Label from "@/Components/FormUtils/Label";
 import { TipoDeUsuariosEnum } from "@/Constants/TipoDeUsuariosEnum";
+import {
+    FaBookOpen,
+    FaUserLock,
+    FaChartArea,
+    FaStore,
+    FaUsers,
+    FaHome,
+    FaRegPaperPlane,
+} from "react-icons/fa";
 export default function Dashboard(props) {
     return (
         <Authenticated auth={props.auth} errors={props.errors}>
@@ -15,47 +24,72 @@ export default function Dashboard(props) {
                             DASHBOARD
                         </h1>
                 </div>
-                <div className="mt-5 grid grid-cols-3 gap-4 max-w-7xl mx-auto sm:px-6 lg:px-8"></div>
+                <div className="mt-5 grid grid-cols-3 gap-4 max-w-7xl mx-auto md:px-6 lg:px-8"></div>
                 {props.auth.user.id_t_user == TipoDeUsuariosEnum.ADMIN && (
-                    <div className="grid sm:grid-cols-5 text-center">
+                    <div className="grid md:grid-cols-3 text-center">
                         <Link href={route("services")} className="w-3/4 sm:w-full mx-auto flex p-3 bg-blue-400 text-center">
                             <div
-                                className="w-full text-center p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
+                                className="flex w-full p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                             border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200"
                             >
-                                <Label className={"text-2xl text-center"}>Servicios</Label>
+                                <div className="flex mx-auto gap-4">
+                                    <Label className={"text-2xl text-center"}>Servicios</Label>
+                                    <span className="grid self-center">
+                                        <FaBookOpen className="scale-150"/>
+                                    </span>
+                                </div>
                             </div>
                         </Link>
                         <Link href={route("users")} className="w-3/4 sm:w-full mx-auto flex p-3 bg-blue-400 text-center">
                             <div
-                                className="w-full text-center p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
+                                className="flex w-full p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                             border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200"
                             >
-                                <Label className={"text-2xl text-center"}>Usuarios</Label>
+                                <div className="flex mx-auto gap-4">
+                                    <Label className={"text-2xl text-center"}>Usuarios</Label>
+                                    <span className="grid self-center">
+                                        <FaUsers className="scale-150"/>
+                                    </span>
+                                </div>
                             </div>
                         </Link>
                         <Link href={route("sales")} className="w-3/4 sm:w-full mx-auto flex p-3 bg-blue-400 text-center">
                             <div
-                                className="w-full text-center p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
+                                className="flex w-full p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                             border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200"
                             >
-                                <Label className={"text-2xl text-center"}>Ventas</Label>
+                                <div className="flex mx-auto gap-4">
+                                    <Label className={"text-2xl text-center"}>Contabilidad</Label>
+                                    <span className="grid self-center">
+                                        <FaStore className="scale-150"/>
+                                    </span>
+                                </div>
                             </div>
                         </Link>
                         <Link href={route("graph")} className="w-3/4 sm:w-full mx-auto flex p-3 bg-blue-400 text-center">
                             <div
-                                className="w-full text-center p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
+                                className="flex w-full p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                             border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200"
                             >
-                                <Label className={"text-2xl text-center"}>Gráficas</Label>
+                                <div className="flex mx-auto gap-4">
+                                    <Label className={"text-2xl text-center"}>Gráficas</Label>
+                                    <span className="grid self-center">
+                                        <FaChartArea className="scale-150"/>
+                                    </span>
+                                </div>
                             </div>
                         </Link>
                         <Link href={route("inactiveUsers")} className="w-3/4 sm:w-full mx-auto flex p-3 bg-blue-400 text-center">
                             <div
-                                className="w-full text-center p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
+                                className="flex w-full p-6 bg-white border-spacing-2 bg-gradient-to-t from-white to-gray-servi
                             border-gray-dark shadow-lg shadow-gray-dark overflow-visible rounded-lg hover:scale-110 ease-in duration-200"
                             >
-                                <Label className={"text-2xl text-center"}>Inactivos</Label>
+                                <div className="flex mx-auto gap-4">
+                                    <Label className={"text-2xl text-center"}>Inactivos</Label>
+                                    <span className="grid self-center">
+                                        <FaUserLock className="scale-150"/>
+                                    </span>
+                                </div>
                             </div>
                         </Link>
 
