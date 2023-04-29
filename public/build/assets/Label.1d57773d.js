3955d7bb91ee033039882163103c8bf9d2d6c0ae
Juan David Quintero <slim.shady99q@gmail.com>
2023-04-29 16:45:23 -0500

    BUILD : npm

---
+++ public/build/assets/Label.1d57773d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app.14a3b57d.js";function n({forInput:t,value:e,className:a,children:l}){return r("label",{htmlFor:t,className:"font-medium text-base "+a,children:r("b",{children:e||l})})}export{n as L};
---
+++ public/build/assets/transportes.ca9f9614.js
@@ -0,0 +1,1 @@
+var s="/build/assets/nombre.22ccf73e.png",a="/build/assets/transportes.a2c83fc4.png";export{s as n,a as t};
