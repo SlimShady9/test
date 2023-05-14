b9be75b6d20ad7792608c563188f9842698d9bed
Juan David Quintero <slim.shady99q@gmail.com>
2023-05-14 16:25:22 -0500

    Gabitroll

--- resources/js/Utils/FetchEmail.js
+++ resources/js/Utils/FetchEmail.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const sendEmail = async (content) => {
     try {
-        axios.post(`/api/email/`, content);
+        axios.post(`/api/email`, content);
     } catch (error) {
         return { data: null, error: error };
     }
